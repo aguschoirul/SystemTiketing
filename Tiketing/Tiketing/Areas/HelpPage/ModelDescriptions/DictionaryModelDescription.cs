f935374df1fcd83a75d35bceed4b5d8eb4da062d
aguschoirul <aguschoirul7@gmail.com>
2020-02-06 18:06:42 +0700

    First Commit

---
+++ Tiketing/Tiketing/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Tiketing.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Tiketing/Tiketing/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Tiketing.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
